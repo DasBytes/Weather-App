1cba0be82d70823d5a619a8c42aef34d767b0877
PD670 <prantadas85463@gmail.com>
2025-06-23 14:30:37 +0600

    changes

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -12,21 +12,27 @@ class _SplashScreenState extends State<SplashScreen> {
   Widget build(BuildContext context) {
     return const Scaffold(
       backgroundColor: Colors.black,
-      body: SafeArea(child: Padding(padding: EdgeInsets.symmetric(vertical: 40)
-      child: Column(children: [
-        Center(child: Text(
-          "Discover The\n Weather In Your City",
-          textAlign: TextAlign.center,
-           style: TextStyle(
-          fontSize: 32,
-          fontWeight: FontWeight.w800,
-          letterSpacing: -0.5,
-          height: 1.2,
-          color: Colors.white,
-
-        ),),)
-      ],),
-      ),
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 40),
+          child: Column(
+            children: [
+              Center(
+                child: Text(
+                  "Discover The\n Weather In Your City",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 32,
+                    fontWeight: FontWeight.w800,
+                    letterSpacing: -0.5,
+                    height: 1.2,
+                    color: Colors.white,
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
